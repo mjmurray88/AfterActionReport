8bcaabcdf5fcecf934276284a4ebacd74c6b85d9
Michael Murray <mjmurray88@gmail.com>
2014-06-15 12:16:23 -0500

    Fix addon object naming

--- Core.lua
+++ Core.lua
@@ -8,13 +8,13 @@ Copyright (c) 2014 Michael J. Murray aka Nydinia of Stormrage(US)
 All rights reserved unless otherwise explicitly stated.
 ]]
 
-local ComboPointsRedux = LibStub("AceAddon-3.0"):NewAddon("AfterActionReport", "AceConsole-3.0")
+local AfterActionReport = LibStub("AceAddon-3.0"):NewAddon("AfterActionReport", "AceConsole-3.0")
 
 --import other necessary libs
 local LSM = LibStub("LibSharedMedia-3.0")
-local L = LibStub("AceLocale-3.0"):GetLocale("ComboPointsRedux")
+local L = LibStub("AceLocale-3.0"):GetLocale("AfterActionReport")
 
-function ComboPointsRedux:OnInitialize()
+function AfterActionReport:OnInitialize()
 	local defaults = {
 		profile = {
 			
@@ -53,19 +53,19 @@ function ComboPointsRedux:OnInitialize()
 	InterfaceOptions_AddCategory(spy)
 end
 
-function ComboPointsRedux:OnEnable()
+function AfterActionReport:OnEnable()
 	
 end
 
-function ComboPointsRedux:Reset()
+function AfterActionReport:Reset()
 	
 end
 
-function ComboPointsRedux:Refresh()
+function AfterActionReport:Refresh()
 	
 end
 
-function ComboPointsRedux:OpenConfig()
+function AfterActionReport:OpenConfig()
 	LoadAddOn("AfterActionReport_Options")
 	LibStub("AceConfigDialog-3.0"):Open("AfterActionReport")
 end
